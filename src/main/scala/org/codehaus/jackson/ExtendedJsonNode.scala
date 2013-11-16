e35172e04ec4059e0990e6c66e9e6f515d2099e5
Devon Stewart <blast@hardchee.se>
2013-11-16 14:36:46 +0900

    Implemement JsonNode's lift and apply
    Since `JsonNode.get(...)` can take either a string (accessing Object
    properties) or an int (accessing array elements), we need a good, typesafe
    way to wrap this overloaded function.
    
    Type classes provide a great way to wrap overloaded functions. Define only
    the types that work, let the compiler catch type errors for you.
    
    --
    
    "Why not just use the overloaded methods?" you might ask. One of the easiest
    cases to make is that it confuses type inference. `node.get _` now needs
    an explicit type.
    
    For more examples, please check out the answers here:
    http://stackoverflow.com/questions/2510108/why-avoid-method-overloading

--- src/main/scala/org/codehaus/jackson/ExtendedJsonNode.scala
+++ src/main/scala/org/codehaus/jackson/ExtendedJsonNode.scala
@@ -5,11 +5,18 @@ import scala.util.{Try,Success,Failure}
 
 import org.codehaus.jackson.JsonNode
 
+trait Extractable[T] {
+  def extract(node: JsonNode, key: T): JsonNode
+}
+
 class ExtendedJsonNode(val node: JsonNode) {
-  def apply[T](key: T): JsonNode = ???
-  def lift[T](key: T): Option[JsonNode] = ???
+  def apply[T](key: T)(implicit ev: Extractable[T]): JsonNode = ev.extract(node, key)
+  def lift[T](key: T)(implicit ev: Extractable[T]): Option[JsonNode] = Option(ev.extract(node, key))
 }
 
 trait JsonNodeImplicits {
+  implicit val extractKey = new Extractable[String] { def extract(node: JsonNode, key: String) = node.get(key) }
+  implicit val extractIndex = new Extractable[Int] { def extract(node: JsonNode, key: Int) = node.get(key) }
+
   implicit def extendJsonNode(node: JsonNode): ExtendedJsonNode = new ExtendedJsonNode(node)
 }
